5c4372e7f016da8ea6f933b93651bfc74c49870a
David King <dking@ketralnis.com>
2018-02-25 14:15:59 -0800

    Bring benchmarks up to date with the API

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,8 +1,9 @@
-#![feature(test)]extern crate test;
+#![feature(test)]
+extern crate test;
 
 extern crate pipelines;
 
-use pipelines::{Pipeline, Mapper, Multiplex};
+use pipelines::{Mapper, Multiplex, Pipeline};
 
 const THREAD_COUNT: usize = 4;
 
@@ -17,40 +18,40 @@ const HEAVY_WORK_FACTOR: u64 = 33;
 const LIGHT_WORK_COUNT: u64 = 100_000;
 const LIGHT_WORK_FACTOR: u64 = 9;
 
-const BUFFSIZE: usize = 5;
-
-
 #[bench]
 fn bench_heavy_single(b: &mut test::Bencher) {
-    b.iter(move || { single(HEAVY_WORK_COUNT, HEAVY_WORK_FACTOR); });
+    b.iter(move || {
+        single(HEAVY_WORK_COUNT, HEAVY_WORK_FACTOR);
+    });
 }
-
 
 #[bench]
 fn bench_light_single(b: &mut test::Bencher) {
-    b.iter(move || { single(LIGHT_WORK_COUNT, LIGHT_WORK_FACTOR); });
+    b.iter(move || {
+        single(LIGHT_WORK_COUNT, LIGHT_WORK_FACTOR);
+    });
 }
-
 
 #[bench]
 fn bench_heavy_multi(b: &mut test::Bencher) {
-    b.iter(move || { multi(HEAVY_WORK_COUNT, HEAVY_WORK_FACTOR); });
+    b.iter(move || {
+        multi(HEAVY_WORK_COUNT, HEAVY_WORK_FACTOR);
+    });
 }
-
 
 #[bench]
 fn bench_light_multi(b: &mut test::Bencher) {
-    b.iter(move || { multi(LIGHT_WORK_COUNT, LIGHT_WORK_FACTOR); });
+    b.iter(move || {
+        multi(LIGHT_WORK_COUNT, LIGHT_WORK_FACTOR);
+    });
 }
-
 
 fn single(work_count: u64, work_factor: u64) {
     let source: Vec<u64> = (1..work_count).collect();
-    Pipeline::new(source, BUFFSIZE)
-        .then(Mapper::new(move |_| fib_work(work_factor)), BUFFSIZE)
+    Pipeline::from(source)
+        .then(Mapper::new(move |_| fib_work(work_factor)))
         .drain();
 }
-
 
 fn multi(work_count: u64, work_factor: u64) {
     let source: Vec<u64> = (1..work_count).collect();
@@ -61,18 +62,14 @@ fn multi(work_count: u64, work_factor: u64) {
         entries.push(Mapper::new(move |_| fib_work(n)))
     }
 
-    Pipeline::new(source, BUFFSIZE)
-        .then(Multiplex::new(entries, BUFFSIZE), BUFFSIZE)
-        .drain();
+    Pipeline::from(source).then(Multiplex::new(entries)).drain();
 }
-
 
 // just something expensive
 fn fib_work(n: u64) -> u64 {
     // fib scales as O(2^n) so `n` can be very coarse to tune
     fib(n) + n
 }
-
 
 fn fib(n: u64) -> u64 {
     if n == 0 || n == 1 {
