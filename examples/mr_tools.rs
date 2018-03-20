a82bd0ad5c281dce5a1ace2c336ed6cdee39de6e
David King <dking@ketralnis.com>
2018-03-19 18:08:02 -0700

    Configureable sorter for mr_tools

--- examples/mr_tools.rs
+++ examples/mr_tools.rs
@@ -6,49 +6,50 @@ use std::io::{self, BufRead, BufReader, BufWriter, Write};
 use std::io::stderr;
 use std::process::{exit, Command, Stdio};
 use std::thread;
+use std::sync::Mutex;
 
 use clap::{App, Arg};
 use pipelines::{Pipeline, PipelineConfig};
 
 fn main() {
     let app = App::new("mr_tools")
-        .arg(
-            Arg::with_name("nmappers")
+        .arg(Arg::with_name("nmappers")
                 .short("M")
                 .help("how many copies of the mapper to run")
                 .takes_value(true)
-                .default_value("1")
-        )
-        .arg(
-            Arg::with_name("nreducers")
+                .default_value("1"))
+        .arg(Arg::with_name("nreducers")
                 .short("R")
                 .help("how many copies of the reducer to run")
                 .takes_value(true)
-                .default_value("1")
-        )
-        .arg(
-            Arg::with_name("field_sep")
+                .default_value("1"))
+        .arg(Arg::with_name("field_sep")
                 .short("F")
                 .required(false)
                 .default_value("\t")
-                .help("field separator (used to group reducer keys; default is tab)"),
-        )
-        .arg(
-            Arg::with_name("mapper")
+                .help("field separator (used to group reducer keys; default is tab)"))
+        .arg(Arg::with_name("mapper")
                 .short("m")
                 .long("map")
                 .takes_value(true)
                 .required(true)
-                .help("command to run as mapper (executed with /bin/sh -c)"),
-        )
-        .arg(
-            Arg::with_name("reducer")
+                .help("command to run as mapper (executed with /bin/sh -c)"))
+        .arg(Arg::with_name("reducer")
                 .short("r")
                 .long("reduce")
                 .takes_value(true)
                 .required(true)
-                .help("command to run as reducer (executed with /bin/sh -c). each reducer also runs its own sorter"),
+                .help("command to run as reducer (executed with /bin/sh -c). each reducer also runs its own sorter"))
+        .arg(Arg::with_name("sort_mem")
+                .short("S")
+                .takes_value(true)
+                .help("-S argument to sort(1) (working memory set)"))
+        .arg(Arg::with_name("sort_cmd")
+                .long("sort")
+                .takes_value(true)
+                .help("the full sort(1) command to run (overrides -S)")
                 );
+
     let arg_matches = app.get_matches();
 
     let mut u_field_sep = arg_matches.value_of("field_sep").unwrap_or("\t");
@@ -85,16 +86,38 @@ fn main() {
         .expect("no reducer")
         .to_owned();
 
-    // prime the pipeline with our stdin
-    let pl = Pipeline::new(|tx| {
-        let stdin = io::stdin();
-        let locked = stdin.lock();
-        for line in locked.split(b'\n') {
-            // TODO check if result is eof?
-            let line = line.expect("bad line in generator");
-            tx.send(line);
+    let sort_cmd = {
+        let sort_mem =
+            arg_matches.value_of_os("sort_mem").map(|s| s.to_owned());
+        let sort_cmd =
+            arg_matches.value_of_os("sort_cmd").map(|s| s.to_owned());
+
+        if sort_cmd.is_some() {
+            sort_cmd.unwrap()
+        } else {
+            let mut sort_cmd = OsString::new();
+            sort_cmd.push("LOCALE=C sort ");
+            if sort_mem.is_some() {
+                sort_cmd.push("-S");
+                sort_cmd.push(sort_mem.unwrap());
+            }
+            sort_cmd
         }
-    }).configure(PipelineConfig::default().batch_size(100));
+    };
+    // shadow it with a reference so multiple threads can all reference it
+    let sort_cmd = Mutex::new(sort_cmd);
+
+    // prime the pipeline with our stdin
+    let pl =
+        Pipeline::new(|tx| {
+            let stdin = io::stdin();
+            let locked = stdin.lock();
+            for line in locked.split(b'\n') {
+                // TODO check if result is eof?
+                let line = line.expect("bad line in generator");
+                tx.send(line);
+            }
+        }).configure(PipelineConfig::default().batch_size(100).buff_size(100));
 
     // execute the mappers
     let pl = pl.ppipe(nmappers, move |tx, rx| {
@@ -138,9 +161,14 @@ fn main() {
     // now execute the reducers. Each of our reducer threads will be sent all of the keys that
     // belong to it, but out of order. So to group them together before they hit the reducer
     // processes each will run its own sorter first
+
     let pl = pl.distribute(nreducers, move |tx, rx| {
         let mut reducer_cmd = OsString::new();
-        reducer_cmd.push("sort | ");
+        {
+            let locked = sort_cmd.lock().unwrap();
+            reducer_cmd.push(&*locked);
+        }
+        reducer_cmd.push(" | ");
         reducer_cmd.push(&reducer);
 
         let child = Command::new("/bin/sh")
--- examples/mr_tools_test.sh
+++ examples/mr_tools_test.sh
@@ -9,14 +9,12 @@ njobs=8
 RUN="cargo run --release --example=mr_tools --"
 
 $RUN \
-    -M${njobs} -R${njobs} \
+    -M${njobs} -R${njobs} -S200m \
     -m 'sed -e "s/[a-zA-Z-]/X/g"' \
     -r "uniq -c" \
     < /usr/share/dict/words \
     | awk '{print $2 " " $1}' \
     | sort -n
-
-
 
 # TODO:
 # lot of unwrap/expects, some kosher some not
